ecec5ab19cb5fbdd0057169c419e7d40ec0ada08
Christopher Cantrell <topherCantrell@gmail.com>
2019-02-08 10:38:39 -0600

    pinout for the mxp port

--- src/subsystems/serial_events.py
+++ src/subsystems/serial_events.py
@@ -1,6 +1,30 @@
 from wpilib.command.subsystem import Subsystem
 from wpilib.serialport import SerialPort
 
+# http://www.ni.com/pdf/manuals/374474a.pdf
+
+'''
+Pinout for the MXP Port
+
+    1 2
+    3 4
+    5 6
+    7 8
+    9 10 UART-RX
+   11 12 DGND      <-- Connect ground
+   13 14 UART-TX   <-- Spew FROM the robot
+   15 16
+   17 18
+   19 20
+   21 22
+   23 24
+   25 26
+   27 28
+   29 30
+   31 32
+   33 34
+'''
+
 
 class SerialEvent(Subsystem):
     """
