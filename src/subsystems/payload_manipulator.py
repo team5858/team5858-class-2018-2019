2426712f4c18dd66b447daf273bf151a7e00f6cb
tophercantrell <tophercantrell@gmail.com>
2019-02-11 19:16:35 -0600

    Added motor controllers for payload manipulator

--- src/subsystems/payload_manipulator.py
+++ src/subsystems/payload_manipulator.py
@@ -1,7 +1,17 @@
 import wpilib
 from wpilib.command.subsystem import Subsystem
 import subsystems
+from ctre.wpi_talonsrx import WPI_TalonSRX
+CAN_ELBOW_LEADER = 3
+CAN_ELBOW_FOLLOWER = 2
+CAN_BALL_INTAKE = 7
 
+def set_motor2(motor, brake):
+    motor.enableCurrentLimit(False)
+    motor.configPeakOutputForward(1, 0)
+    motor.configPeakOutputReverse(-1, 0)
+    motor.setNeutralMode(brake)
+    motor.configOpenLoopRamp(0, 0)
 
 class Payload(Subsystem):
     """
@@ -12,6 +22,14 @@ class Payload(Subsystem):
         super().__init__("Payload")
         # TODO Two motors ... one leader, one follower
         # TODO Two limit switches on the follower
+
+        self.elbowleader = WPI_TalonSRX(CAN_ELBOW_LEADER)
+        self.elbowleader.setInverted(False)
+        set_motor2(self.elbowleader, WPI_TalonSRX.NeutralMode.Brake)
+
+        self.elbowfollower = WPI_TalonSRX(CAN_ELBOW_FOLLOWER)
+        self.elbowfollower.setInverted(False)
+        set_motor2(self.elbowfollower, WPI_TalonSRX.NeutralMode.Brake)
 
     def wheels_in(self):
         # TODO Spin motors to bring ball in
@@ -32,3 +50,9 @@ class Payload(Subsystem):
     def hatch_punch_in(self):
         # TODO turn off air to piston
         pass
+    
+    def elbow_down(self):
+        self.elbowleader.speed = 1
+
+    def elbow_up(self):
+        self.elbowleader.speed = 0
