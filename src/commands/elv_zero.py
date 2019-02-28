449ae86e0dd39cbe29a51bd7d97247bbdc6094fe
tophercantrell <tophercantrell@gmail.com>
2019-02-28 17:16:52 -0600

    Merge branch 'master' of https://github.com/team5858/team5858-class-2018-2019
    # Conflicts:
    #	.idea/workspace.xml
    #	src/subsystems/elevator.py

---
+++ src/commands/elv_zero.py
@@ -0,0 +1,17 @@
+from wpilib.command import Command
+import subsystems
+
+class elv_zero(Command):
+
+    def __init__(self):
+        super().__init__("")
+
+        self.requires(subsystems.ELEVATOR)
+
+    def execute(self):
+        #subsystems.ELEVATOR.check_for_zero()
+        pass
+
+    def isFinished(self):
+        # This is always running
+        return False
